5a8212b06185bd69cbc7a4fc4cf53d34969cddfc
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-23 14:47:05 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,3 @@
-CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
-
-INSERT INTO groceries VALUES (1, "Apples", 2, 1);
-INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
-INSERT INTO groceries VALUES(3, "Peaches", 6, 3);
+CREATE TABLE projects (id INTEGER PRIMARY KEY)
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, project_id INTEGER, user_id INTEGER)
