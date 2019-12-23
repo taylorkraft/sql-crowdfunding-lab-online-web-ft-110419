3b3a337121cf2a2167f6e82d27760e8b20996897
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-23 18:55:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,8 +35,8 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount)
   FROM users
-  INNER JOIN pledges
-  ON users.name = pledges.user_id
+  JOIN pledges
+  ON users.id = pledges.user_id
   GROUP by users.name
   ORDER BY SUM(pledges.amount);"
 end
