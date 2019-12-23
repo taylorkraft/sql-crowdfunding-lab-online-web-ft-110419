be985975383c8cc5fef55266c067593af144fde0
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-23 19:08:10 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,7 +46,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   FROM projects
   JOIN pledges
   ON projects.id = pledges.project_id
-  GROUP BY projects.category 
+  GROUP BY projects.category
   HAVING project.category = music;"
 end
 
