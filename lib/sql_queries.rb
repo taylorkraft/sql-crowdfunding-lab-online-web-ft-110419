073ef672acc2532e55b2138226620430f514f670
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-23 18:50:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,11 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  "SELECT users.name, pledges.amount
+  FROM users
+  JOIN pledges ON users.name = pledges.user_id
+  GROUP by users.name
+  ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
